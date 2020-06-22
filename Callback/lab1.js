83c57b51b707d3eddb75f82dd511243df2aedfcb
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-22 14:30:34 +0700

    modified lab1

--- Callback/lab1.js
+++ Callback/lab1.js
@@ -1,3 +1,20 @@
-setTimeout(() => console.log('A'), 1000)
-setTimeout(() => console.log('B'), 2000)
-setTimeout(() => console.log('C'), 3000)
+// setTimeout(() => console.log('A'), 1000)
+// setTimeout(() => console.log('B'), 2000)
+// setTimeout(() => console.log('C'), 3000)
+// setTimeout(() => console.log('D'), 4000)
+
+// CallBack hell
+
+setTimeout(() => {
+    console.log('A')
+    setTimeout(() => {
+        console.log('B')
+        setTimeout(() => {
+            console.log('C')
+            setTimeout(() => {
+                console.log('D')
+            },1000)
+        },1000)
+    }, 1000)
+}, 1000)
+
