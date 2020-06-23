fda6898e3d503f7bf2496f12ce268aae75877534
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-23 17:04:51 +0700

    added the todoList backend tutorial

---
+++ todo-backend/controllers/todoList.js
@@ -0,0 +1,36 @@
+const { uniqueId } = require("lodash")
+
+let todoList = []
+
+const getTodoList = (req,res) => {
+    res.status(200).send(todoList)
+}
+const createTodoList = (req,res) => {
+    const newTodo = {
+        id:uniqueId(),
+        task:req.body.task
+    };
+    todoList.push(newTodo)
+   res.status(201).send(newTodo)
+}
+const updateTodoList = (req,res) => {
+    const targetId = String(req.params.id);
+    const targetIdx = todoList.findIndex(todo => todo.id === targetId)
+    todoList[targetIdx] = {
+        id:targetId,
+        task:req.body.task
+    }
+    res.status(200).send({message:'Successfully updated'})
+}
+const deleteTodoList = (req,res) => {
+    const targetId = String(req.params.id)
+    todoList = todoList.filter(todo => todo.id !== targetId)
+    res.status(204).send()
+}
+
+module.exports = {
+    createTodoList,
+    getTodoList,
+    updateTodoList,
+    deleteTodoList
+}
