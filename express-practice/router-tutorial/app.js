f3c7c453628d0b35a75f37eb872ad0f3c16f02f7
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-23 16:19:39 +0700

    added express tutorial -> post

--- express-practice/router-tutorial/app.js
+++ express-practice/router-tutorial/app.js
@@ -3,4 +3,9 @@ const app = express()
 
 const userRoute = require('./userRoute')
 app.use('/user',userRoute)
+
+app.use('/pub',express.static('./public'))
+app.get('/',(req,res) => res.redirect('/pub/form1.html'))
+app.get('/sample',(req,res) => res.redirect('/pub/sample.json'))
+
 app.listen(3000, () => console.log('server start...'))
