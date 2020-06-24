3a4f0aaf6952e82b645a4430c8b9c85f924cf16b
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-24 12:36:08 +0700

    added instructions to the home page

--- Cards-proj/cards.js
+++ Cards-proj/cards.js
@@ -18,23 +18,32 @@ const singleCard = (i) => {
     </div>`
 }
 app.get('/', (req, res) => {
-    let homePage = `
-    <h1>Welcome Home!</h1>
+    let homePage = `<div style="display:flex;justify-content:center;align-items:center;flex-direction:column">
+    <h1>Welcome to the Home Page!</h1>
+    <li style="font-size:24px;padding:10px">To view 'All' Users go to /all</li>
+    <li style="font-size:24px;padding:10px">To view Users from their Ids go to /id</li>
+    <li style="font-size:24px;padding:10px">To view Search for users using their First Names go to /search</li>
+    </div>
     `
     res.send(homePage)
 })
 
 app.get('/search',(req,res) => {
-    let searchItem =  `<form action="/dosearch" method="GET">
+    let searchItem =  `<div style="display:flex;justify-content:center;align-items:center;flex-direction:column">
+    <h1>Welcome to Search!</h1>
+    <h3>Search for users information using their First Names </h3>
+    <form action="/dosearch" method="GET" style="text-align:center;margin:20px auto;">
         <label for="firstname">First Name</label>
         <input type="text" name="firstname" placeholder="First Name">
         <input type="submit">
-    </form>`
-    res.send(searchItem)
+    </form></div>`
+    res.send(searchItem) //send a request to dosearch
 })
 
+// Responds to dosearch request
 app.get('/dosearch',(req,res) => {
     let currentUser = userArr.filter(x=> x.firstName.includes(req.query.firstname))
+
     let cardItems = 
     `<div style="margin:30px auto;width:500px;height:400px;padding:20px;box-shadow: 0 1px 5px rgba(0,0,0,0.3)">
             ${cardInside(currentUser[0].userId)}
