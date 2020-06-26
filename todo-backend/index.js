12009d6f835e8e00ad43cd8a276c2d5243b2eee8
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-26 10:57:38 +0700

    modified todolist to get data from database

--- todo-backend/index.js
+++ todo-backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const todoListRoutes = require('./routes/todoList')
+const db = require('./models')
 
 app.use(cors())
 app.use(express.urlencoded({extended:false}))
@@ -10,4 +11,6 @@ app.use(express.json())
 
 app.use('/todo-list',todoListRoutes)
 
-app.listen(8000, () => console.log("Server is running at port 8000"))
+db.sequelize.sync().then(() => {
+    app.listen(8000, () => console.log("Server is running at port 8000"))
+})
